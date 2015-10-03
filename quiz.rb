03d2ef171d1bb6438ffae6c56f9ee4468d486206
Ruben Davila <ruben@crushpath.com>
2015-10-03 11:06:27 -0500

    Return unansered questions first and complete with random questions.

--- quiz.rb
+++ quiz.rb
@@ -4,7 +4,7 @@ QUESTION_FIELDS = %w(strand_id strand_name standard_id standard_name question_id
 USAGE_FIELDS = %w(student_id question_id assigned_hours_ago answered_hours_ago)
 
 Question = Struct.new(*QUESTION_FIELDS.map(&:to_sym))
-Usage = Struct.new(*QUESTION_FIELDS.map(&:to_sym))
+Usage = Struct.new(*USAGE_FIELDS.map(&:to_sym))
 
 def generate_quiz(s_id = nil, total = nil)
   puts 'Please enter your student id:' if s_id.nil?
@@ -15,12 +15,16 @@ def generate_quiz(s_id = nil, total = nil)
   total = total || gets.to_i
   return generate_quiz(student_id) if total <= 0
 
-  select_questions(total)
+  select_questions(student_id.chomp, total)
 end
 
-def select_questions(total)
+def select_questions(student_id, total)
+  answered_question_ids = usages.select { |u| u.student_id.to_s == student_id }.map(&:question_id)
+  results =  questions.select { |q| answered_question_ids.include?(q.question_id) }[0..(total - 1)]
+  results << questions.sample(results.size - total) if results.size < total
+
   puts "Question ids are:"
-  puts questions.sample(total).map(&:question_id)
+  puts results.map(&:question_id)
 end
 
 def questions
